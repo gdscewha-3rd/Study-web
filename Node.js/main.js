ad123b596db1372c17b09dc9677b425ade8620c1
flowersayo <flowersayo0811@ewhain.net>
2021-11-16 07:58:39 +0900

    [11월 16일] Node.js

--- Node.js/main.js
+++ Node.js/main.js
@@ -1,83 +1,111 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url'); //url이라는 모듈 사용.
+var url = require('url'); 
 
+//HTML 템플릿 생성
+function templateHTML(title,list,body){
+
+return  `<!doctype html>
+<html>
+<head>
+  <title>WEB1-${title} </title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1> 
+  ${list}
+  <a href="/create">create</a>
+  ${body}
+  </p>
+</body>
+</html>
+`;
+
+}
+
+// List 목록 생성
+function templateList(filelist){
+
+  var list='<ul>'; 
+        for(var i=0;i<filelist.length;i++){
+          
+          list+=`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+
+        list +='</ul>'; 
+
+        return list;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    console.log(_url);
-    var queryData =url.parse(_url,true).query; // 객체 형태로 query string저장
-    var pathname =url.parse(_url,true).pathname;
     
-   
-   //console.log(pathname);
+    var queryData =url.parse(_url,true).query; 
+    var pathname =url.parse(_url,true).pathname;
+    console.log(pathname);
    if(pathname==='/'){ // 경로명이 /라면
 
     //main home 처리(WEB)
     if (queryData.id===undefined){ // querystring이 없다면
-       
-       fs.readFile(`data/${queryData.id}`,'utf-8',(err, description)=>{
+      console.log(1);
+      fs.readdir('./data',function(error,filelist){
         var title = 'Welcome';
         var description='Hello,WEB';
-        var template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title} </title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1> 
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=Javascript">JavaScript</a></li>
-          </ol>
-          <h2> ${title}</h2>
-          <p>
-          ${description}
-          </p>
-        </body>
-        </html>
-        `;
+       
+      var list=templateList(filelist);
+        var template =templateHTML(title,list,`<h2>${title}</h2>${description}`);
         response.writeHead(200);
-        response.end(template); //template을 화면상에 그려낸다.
-    });
+        response.end(template); 
+      })
+        
+    
    }
-   else{ // 경로명이 /가 아니라면
+   else{ // querystring이 있다면
+
+    fs.readdir('./data',function(error,filelist){
+      var title = 'Welcome';
+      var description='Hello,WEB';
+    
 
     fs.readFile(`data/${queryData.id}`,'utf-8',(err, description)=>{
         var title = queryData.id;
-        var template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title} </title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1> 
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=Javascript">JavaScript</a></li>
-          </ol>
-          <h2> ${title}</h2>
-          <p>
-          ${description}
-          </p>
-        </body>
-        </html>
-        `;
+        var list = templateList(filelist);
+        var template =templateHTML(title,list,`<h2>${title}</h2>${description}`);
         response.writeHead(200);
         response.end(template); //template을 화면상에 그려낸다.
+       });
+     });
+     }
+
+  }
+
+  else if (pathname==='/create'){
+ 
+    fs.readdir('./data',function(error,filelist){
+      var title = 'Web-create'; 
+      var list=templateList(filelist);
+      var template =templateHTML(title,list,`<form action="http://localhost:3000/process_create" method="post">
+      <p> <input type="text" name="title" placeholder="title"></p>
+      <p>
+          <textarea name="contents" placeholder="contents"></textarea>
+      </p>
+      <p>
+          <input type="submit">
+      </p>
+      
+      </form>`);
+      response.writeHead(200);
+      response.end(template); 
     });
-    }
+      
 
-   }
+  }
 
-   else {
+   else { // 경로명이 /가 아니라면 ex) localhost:3000/asfsd
+ 
     response.writeHead(404);
     response.end('Not found');
-}
+    }
   
    
  
