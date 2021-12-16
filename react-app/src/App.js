3b7087b7135e69a60d1003a51dd28e9aa99c0d0b
flowersayo <flowersayo0811@ewhain.net>
2021-12-16 19:16:58 +0900

    [12월 16일] React폴더 생성
    기본 프로젝트 폴더 생성

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css'; // App 컴포넌트의 디자인
+import { Component } from 'react/cjs/react.production.min';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        Hello, yeonleaf
+      </div>
+    )
+  }
+}
+export default App;
