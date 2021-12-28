e9556eda09d632bcacf4d6d04f65a44d1192e5f0
Seojin Jang <82519477+SEOJIN-Lab@users.noreply.github.com>
2021-12-28 17:16:11 +0900

    React(2)

--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class TOC extends Component{
     render(){
       console.log('TOC render');
@@ -7,7 +6,17 @@ class TOC extends Component{
       var data = this.props.data;
       var i = 0;
       while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        lists.push(
+          <li key={data[i].id}>
+            <a 
+              href={"/content/"+data[i].id}
+              data-id={data[i].id}
+              onClick={function(e){
+                e.preventDefault();
+                this.props.onChangePage(e.target.dataset.id);
+              }.bind(this)}
+            >{data[i].title}</a>
+          </li>);
         i = i + 1;
       }
       return (
